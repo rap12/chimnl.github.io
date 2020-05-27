09b579f70f87d66177ec6a895287ecefa087aa57
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-05-28 00:20:42 +0800

    added execom page

---
+++ assets/javascript/member.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#member-close').click(function() {
+        $('#member-overlay').hide();
+        $('body').removeClass('stiff');
+        $('.navbar').removeClass('z');
+        $('#see').css("display", "block");
+    });
+
+    $('#member-click-off').click(function() {
+        $('#member-overlay').hide();
+        $('body').removeClass('stiff');
+        $('.navbar').removeClass('z');
+        // $('#see').css("display", "block");
+    });
+
+    $('.user a').click(function() {
+        var index = $('.user a').index(this);
+        console.log(index);
+        $('body').addClass('stiff');
+        $('.navbar').addClass('z');
+        $('#member-overlay').show();
+        $('#member-container #member-content').empty();
+        $('.user').eq(index).clone().appendTo('#member-content');
+        $('#member-container *').removeClass('d-none');
+        $('#member-container .user-bio').removeClass('module');
+        $('#member-container .user-bio').removeClass('overflow');
+        $('#member-container .user').removeClass('shadow-sm');
+        $('#member-container .user').removeClass('user');
+        $('#member-container p').addClass('text-center');
+        $('#member-container .user-bio').addClass('mb-3');
+        $('#member-container a').addClass("hidden");
+
+        // let socials = $.parseHTML($('#modal-container .socials-wrapper').clone().text());
+
+        // $('#modal-container .socials-wrapper').empty();
+        // $('#modal-container .socials-wrapper').html(socials);
+        // $('#modal-container .socials-wrapper').addClass('d-flex justify-content-center margin-y-24');
+        // $('#modal-container .socials-wrapper i').addClass('fa-2x margin-x-16');
+    });
+});
---
+++ executive-committee.md
@@ -0,0 +1,25 @@
+---
+layout: default
+title: CHI MNL Executive Committee
+---
+{% include member-info.html %}
+
+<h3 class = "font-weight-bold text-center mt-5">Executive Committee</h3>
+
+<section class = "container mt-5">
+    <div class = "d-flex flex-wrap justify-content-center">
+    <!-- {% assign members = site.data.execom.committee %} -->
+        {% for member in site.data.execom %}
+        <div class = "user shadow-sm bg-white m-3 text-center">
+            <div class="user-img bg-secondary mx-auto margin-bottom-16" style="background: url( {{ member.image }} ); background-position: center; background-size: cover;"></div>
+            <p class = "position mt-2 m-0 text-center">{{ member.position }}</p>
+            <p class = "text-center m-0 member-name">{{ member.name }}</p>
+            <div class = "user-bio module overflow">
+                <p class =" mt-3 m-0 text-center">{{ member.bio }}</p>
+            </div>
+            <a href = "#" class ="btn btn-primary mt-3 more">See more</a>
+        </div>
+        {% endfor %}
+    </div>
+
+<section>
